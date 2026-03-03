0e2dd4aca47e38090d160ef0150af7fc47df595a
Abdullrahman Al-Akhzami <96819774+AAkhzami@users.noreply.github.com>
2026-03-03 13:56:01 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Tadhkir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudMinute = new System.Windows.Forms.NumericUpDown();
+            this.nudSecond = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Linen;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.nudMinute);
+            this.panel1.Controls.Add(this.nudSecond);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 243);
+            this.panel1.TabIndex = 15;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(323, 281);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(214, 43);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(86, 281);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(214, 43);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudMinute
+            // 
+            this.nudMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudMinute.Location = new System.Drawing.Point(176, 104);
+            this.nudMinute.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nudMinute.Name = "nudMinute";
+            this.nudMinute.Size = new System.Drawing.Size(120, 34);
+            this.nudMinute.TabIndex = 37;
+            this.nudMinute.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudSecond
+            // 
+            this.nudSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudSecond.Location = new System.Drawing.Point(327, 104);
+            this.nudSecond.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.nudSecond.Name = "nudSecond";
+            this.nudSecond.Size = new System.Drawing.Size(120, 34);
+            this.nudSecond.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "minute";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "second";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(622, 358);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tadhkir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSecond)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudMinute;
+        private System.Windows.Forms.NumericUpDown nudSecond;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tadhkir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            CheckTimeSet();
+            timer1.Start();
+        }
+
+        void CheckTimeSet()
+        {
+            int second = ((int)nudSecond.Value) * 1000;
+            int Min = ((int)nudMinute.Value) * 60000;
+            int result = Min + second;
+
+            timer1.Interval = result;
+            timer1.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckTimeSet();
+            timer1.Stop();
+            notifyIcon1.Visible = true;
+            notifyIcon1.Icon = SystemIcons.Information;
+        }
+        
+        string GetRandomText()
+        {
+            string[] adhkar = new string[]
+                    {
+                        "أستغفر الله",
+                        "أستغفر الله العظيم",
+                        "أستغفر الله العظيم وأتوب إليه",
+                        "اللهم اغفر لي وتب عليّ إنك أنت التواب الرحيم",
+                        "رب اغفر لي ولوالديّ",
+                        "اللهم إنك عفو تحب العفو فاعفُ عني",
+                        "أستغفر الله الذي لا إله إلا هو الحي القيوم وأتوب إليه",
+                        "اللهم أنت ربي لا إله إلا أنت خلقتني وأنا عبدك وأنا على عهدك ووعدك ما استطعت أعوذ بك من شر ما صنعت أبوء لك بنعمتك عليّ وأبوء بذنبي فاغفر لي فإنه لا يغفر الذنوب إلا أنت",
+                        "سبحان الله",
+                        "سبحان الله وبحمده",
+                        "سبحان الله العظيم",
+                        "سبحان الله وبحمده سبحان الله العظيم",
+                        "سبحان الله عدد خلقه",
+                        "سبحان الله ملء السماوات وملء الأرض",
+                        "سبحان الله والحمد لله",
+                        "سبحان الله ولا حول ولا قوة إلا بالله",
+                        "الله أكبر",
+                        "الله أكبر كبيرًا",
+                        "الله أكبر ولله الحمد",
+                        "الله أكبر الله أكبر لا إله إلا الله، الله أكبر الله أكبر ولله الحمد",
+                        "الله أكبر كبيرًا والحمد لله كثيرًا وسبحان الله بكرةً وأصيلًا",
+                        "لا إله إلا الله",
+                        "لا إله إلا الله وحده لا شريك له له الملك وله الحمد وهو على كل شيء قدير",
+                        "الحمد لله",
+                        "الحمد لله رب العالمين",
+                        "لا حول ولا قوة إلا بالله",
+                        "حسبنا الله ونعم الوكيل",
+                        "توكلت على الله",
+                        "اللهم صلِّ وسلم على نبينا محمد"
+                    };
+
+            Random r = new Random();
+            string Text = adhkar[r.Next(0,adhkar.Length -1)];
+            return Text;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            notifyIcon1.ShowBalloonTip(1000,"Tadhkir",GetRandomText(),ToolTipIcon.Info);
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+    }
+}
